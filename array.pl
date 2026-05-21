bf92a7060427e9f1cbf176e1788565eacca6f1e5
KameRamen <hiroki.hatanaka@proton.me>
2026-05-21 15:56:12 +0900

    array.pl

---
+++ array.pl
@@ -0,0 +1,18 @@
+﻿use v5.38;
+
+my $leader = "\n......\n\n";
+
+my @fruits = ("apple", "orange", "banana", "grape");
+
+say "First fruit: $fruits[0]";
+say "All: @fruits";
+
+print $leader;
+
+push @fruits, "strawberry";
+
+say "All (updated): @fruits";
+
+print $leader;
+
+say "TOTAL: ".scalar(@fruits)." fruits";
