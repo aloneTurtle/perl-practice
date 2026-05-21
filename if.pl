b7c3323fc961493da0665c1d3ef31b9bb39054eb
KameRamen <hiroki.hatanaka@proton.me>
2026-05-22 00:42:01 +0900

    if.pl

---
+++ if.pl
@@ -0,0 +1,13 @@
+﻿use v5.38;
+
+my @messages = ("Good morning!", "Hello!", "Good night!");
+
+my $currentTime = 2; # Change this value
+
+if ($currentTime == 0) {
+    say $messages[0];
+} elsif ($currentTime == 2) {
+    say $messages[2];
+} else {
+    say $messages[1];
+};
