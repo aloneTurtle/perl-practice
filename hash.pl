c33e7049b21b7fb530cb07b791020e4988b611ec
KameRamen <hiroki.hatanaka@proton.me>
2026-05-21 16:18:12 +0900

    hash.pl

---
+++ hash.pl
@@ -0,0 +1,17 @@
+﻿use v5.38;
+
+my %person = (
+    name     => "KameRamen",
+    age      => 100,
+    hobby    => "Programming",
+    language => "Japanese"
+);
+
+say "Name: $person{name}";
+say "Age: $person{age}";
+say "Hobby: $person{hobby}";
+say "Language: $person{language}";
+
+$person{city} = "Tokyo";
+
+say "Ciry: $person{city}";
